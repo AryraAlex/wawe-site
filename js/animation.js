67957fe7f8e0fb1fd9ab015ee04f56e4bb2d070b
AryraAlex <mrzombi.com112@gmail.com>
2025-01-25 00:40:36 +0700

    feat: add burger button and js for burger button, refactor, adaptive

--- js/animation.js
+++ js/animation.js
@@ -1,21 +1,25 @@
-window.addEventListener('scroll', () => {
-const scrollPos = window.scrollY
-const bgNav = document.querySelector('.header-nav__bg')
-if (scrollPos > 400) {
-  bgNav.classList.add('bg-visible')
-}
-else {
-  bgNav.classList.remove('bg-visible')
-};
-})
+window.addEventListener("scroll", () => {
+  const scrollPos = window.scrollY;
+  const bgNav = document.querySelector(".header-nav__bg");
 
-window.addEventListener('scroll', () => {
-  const scrollPos = window.scrollY
-  const title = document.querySelector('.header-title')
-  if (scrollPos > 200) {
-    title.classList.add('title')
+  if (scrollPos > 650) {
+    bgNav.classList.add("bg-visible");
+  } else {
+    bgNav.classList.remove("bg-visible");
   }
-  else {
-    title.classList.remove('title')
-  };
-  })
+});
+
+window.addEventListener("scroll", () => {
+  const scrollPos = window.scrollY;
+  const title = document.querySelector(".header-title");
+
+  if (scrollPos > 200) {
+    title.classList.add("title");
+  } else {
+    title.classList.remove("title");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  document.body.style.cssText = `--scrollTo: ${this.scrollY}px`;
+});
